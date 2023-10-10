bddd899b242491658d6a4e004a29978e832e5edd
FelipeMDantas <felipemdantas@terra.com.br>
2023-10-09 23:28:34 -0300

    Scrapping from Amazon successfully carried out.

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -18,8 +19,24 @@ export async function scrapeAmazonProduct(url: string) {
 
   try {
     const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-    console.log(response.data);
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listprice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    console.log({ title, currentPrice, originalPrice });
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
