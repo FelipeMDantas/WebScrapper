1a40008027e898ca6e87975a6279cbe4663fa2a1
FelipeMDantas <felipemdantas@terra.com.br>
2023-10-12 23:25:55 -0300

    Mongoose Product model created.

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,1 +1,19 @@
-export const connectToDB = async () => {};
+import mongoose from "mongoose";
+
+let iscConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URI)
+    return console.log("MONGODB_URI is not defined");
+
+  if (iscConnected) return console.log("=> using existing database connection");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    iscConnected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
