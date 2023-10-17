02dc47d80a3680e12d2169f7d7f558b4b6e9bfd6
FelipeMDantas <felipemdantas@terra.com.br>
2023-10-16 22:21:40 -0300

    PriceInfoCard component created.

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,4 +1,6 @@
+import PriceInfoCard from "@/components/PriceInfoCard";
 import { getProductById } from "@/lib/actions";
+import { formatNumber } from "@/lib/utils";
 import { Product } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
@@ -27,7 +29,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
         </div>
 
         <div className="flex-1 flex flex-col">
-          <div className="flex justify-between items-start gap-5 flex-wrap">
+          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
             <div className="flex flex-col gap-3">
               <p className="text-[28px] text-secondary font-semibold">
                 {product.title}
@@ -64,6 +66,72 @@ const ProductDetails = async ({ params: { id } }: Props) => {
                   height={20}
                 />
               </div>
+
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src="/assets/icons/share.svg"
+                  alt="share"
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="product-info">
+            <div className="flex flex-col gap-2">
+              <p className="text-[34px] text-secondary font-bold">
+                {product.currency} {formatNumber(product.currentPrice)}
+              </p>
+              <p className="text-[21px] text-black opacity-50 line-through">
+                {product.currency} {formatNumber(product.originalPrice)}
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-3">
+                <div className="product-stars">
+                  <Image
+                    src="/assets/icons/star.svg"
+                    alt="star"
+                    width={16}
+                    height={16}
+                  />
+                  <p className="text-sm text-primary-orange font-semibold">
+                    {product.stars || "25"}
+                  </p>
+                </div>
+
+                <div className="product-reviews">
+                  <Image
+                    src="/assets/icons/comment.svg"
+                    alt="comment"
+                    width={16}
+                    height={16}
+                  />
+                  <p className="text-sm text-secondary font-semibold">
+                    {product.reviewsCount} Reviews
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-sm text-black opacity-50">
+                <span className="text-primary-green font-semibold">93%</span> of
+                buyers have recommended this.
+              </p>
+            </div>
+          </div>
+
+          <div className="my-7 flex flex-col gap-5">
+            <div className="flex gap-5 flex-wrap">
+              <PriceInfoCard
+                title="Current Price"
+                iconSrc="/assets/icons/price-tag,svg"
+                value={`${product.currency} ${formatNumber(
+                  product.currentPrice
+                )}`}
+                borderColor="#b6dbff"
+              />
             </div>
           </div>
         </div>
