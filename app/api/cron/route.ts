8e8568100442169f71ee08c8f8d9c0047627d135
FelipeMDantas <felipemdantas@terra.com.br>
2023-10-23 23:55:50 -0300

    Product's status verification function coded.

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,5 +1,6 @@
 import Product from "@/lib/models/product.model";
 import { connectToDB } from "@/lib/mongoose";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import {
   getAveragePrice,
@@ -7,6 +8,7 @@ import {
   getHighestPrice,
   getLowestPrice,
 } from "@/lib/utils";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
@@ -44,8 +46,30 @@ export async function GET() {
           scrapedProduct,
           currentProduct
         );
+
+        if (emailNotifType && updatedProduct.users.length > 0) {
+          const productInfo = {
+            title: updatedProduct.title,
+            url: updatedProduct.url,
+          };
+
+          const emailContent = await generateEmailBody(
+            productInfo,
+            emailNotifType
+          );
+
+          const userEmails = updatedProduct.users.map(
+            (user: any) => user.email
+          );
+
+          await sendEmail(emailContent, userEmails);
+        }
+
+        return updatedProduct;
       })
     );
+
+    return NextResponse.json({ message: "Ok", data: updatedProducts });
   } catch (error) {
     throw new Error(`Error in GET: ${error}`);
   }
